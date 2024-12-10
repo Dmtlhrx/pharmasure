e0caf94fc6078170676643f680f8b915ead792b7
Mouhsinou <mamamouhsinou@gmail.com>
2024-12-10 09:19:32 +0100

    userversion

--- ilerasure/src/App.js
+++ ilerasure/src/App.js
@@ -1,29 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HeaderSection from "./components/Header";
-import Home from "./components/pages/home";
-import Login from "./components/pages/login";
-import Register from "./components/pages/register";
-import PharmacyDashboard from "./components/pages/PharmacyDashboard";
-import DoctorDashboard from "./components/pages/DoctorDashboard";
-import DashboardUser from "./components/pages/DashboardUser";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-       
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/PharmacyDashboard" element={<PharmacyDashboard />} />
-          <Route path="/DoctorDashboard" element={<DoctorDashboard />} />
-          <Route path="/DashboardUser" element={<DashboardUser />} />
-
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
